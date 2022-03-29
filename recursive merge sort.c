fe4423817473629d93e476d022e29f7e9f61b604
Ashutosh2609Mishra <89624895+Ashutosh2609Mishra@users.noreply.github.com>
2022-03-29 09:29:54 +0530

    Add files via upload

---
+++ recursive merge sort.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include <limits.h>
+void Merge(int A[],int l,int mid,int h){
+	int i=l,j=mid+1,k=l;
+	int B[100];
+	while(i<=mid && j<=h){
+		if(A[i]<A[j])
+			B[k++]=A[i++];
+		else
+			B[k++]=A[j++];
+	}
+	for(;i<=mid;i++)
+		B[k++]=A[i];
+	for(;j<=h;j++)
+		B[k++]=A[j];
+	for(i=l;i<=h;i++)
+		A[i]=B[i];
+}
+void MergeSort(int A[],int l,int h){
+	int mid;
+	if(l<h){
+		mid=(l+h)/2;
+		MergeSort(A,l,mid);
+		MergeSort(A,mid+1,h);
+		Merge(A,l,mid,h);
+	}
+}
+int main(){
+	int A[]={3,7,9,10,6,5,12,4,11,2},n=10,i;
+	MergeSort(A,0,n-1);
+	for(i=0;i<n-1;i++){
+		printf("%d ",A[i]);
+	}
+	return 0;
+}
